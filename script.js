d4dd4a19490093a8a426727eef41f371529d9830
Chowiee <165966007+Chiwiee@users.noreply.github.com>
2024-07-22 10:01:28 +0800

    Create createSquare function

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const mainContainer = document.querySelector("#main-container");
 const gridContainer = document.querySelector("#grid-container");
+
+function createSquare() {
+  for (let i = 1; i <= 16 * 16 || userInput * userInput; i++) {
+    const square = document.createElement("div");
+    square.id = "square";
+    gridContainer.appendChild(square);
+  }
+}
+createSquare();
